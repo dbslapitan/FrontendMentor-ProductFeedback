64a16f9afe6756e5af36ce70525f83a356cafdc1
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2022-05-16 04:36:02 -0400

    Brand design complete.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './navigation/brand/brand.component';
+import { CategoryComponent } from './navigation/category/category.component';
+import { RoadMapComponent } from './navigation/road-map/road-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    CategoryComponent,
+    RoadMapComponent
   ],
   imports: [
     BrowserModule,
