0595d0e718d9e11edcf999ad510d34c9fe161573
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2022-05-16 22:06:52 -0400

    Finish UI for Category.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'product-feedback';
+
+  toggleNavigation(){
+    console.log('DIV Clicked!!!');
+    const svg = document.querySelector('.hamburger')!;
+    const navContainer = document.querySelector('.nav-container')!;
+
+    svg.classList.toggle('toggle');
+    navContainer.classList.toggle('toggle');
+  }
 }
